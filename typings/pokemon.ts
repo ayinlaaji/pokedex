cbba623f1c96ecdc49798ba0caac4275bf8fc421
Ayinla Abdulsalam <ayinlaabdulsalam@gmail.com>
2020-03-13 12:19:03 +0000

    chore(): add typings

---
+++ typings/pokemon.ts
@@ -0,0 +1,10 @@
+export type Pokemon = {
+  height: number;
+  weight: number;
+  name: string;
+  id: string;
+  baseExperience: number;
+  imgUrl: string;
+  types: string[];
+  abilities: string[];
+};
